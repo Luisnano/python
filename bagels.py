63585a20cf468a3932a463c34df5a51169ee1c6b
Luis <luis.torresmo@alum.uca.es>
2022-04-25 13:16:06 +0200

    bagels_inicio

---
+++ bagels.py
@@ -0,0 +1,5 @@
+#Juego de adivinar numeros de 3 cifras. 
+#   -Si tiene un digito bien pero mal colocado:         Pico
+#   -Si tiene un digito bien y en posicion correcta:    Fermi
+#   -Si no tiene ningun digito bien :                   Bagel
+
